2ae1925833fdaa8b97bc7092778b88b62d99e9cc
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-01 13:07:17 +0300

    N queens
    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.
    
    Usage: nqueens N
    If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
    where N must be an integer greater or equal to 4
    If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
    If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
    The program should print every possible solution to the problem
    One solution per line
    Format: see example
    You don’t have to print the solutions in a specific order
    You are only allowed to import the sys module

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""N Queens"""
+
+import sys
+
+def is_safe(board, row, col, n):
+    # Check if no queen can attack this position
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(n):
+    if n < 4:
+        print("N must be at least 4")
+        return
+
+    def print_solution(board):
+        for row in board:
+            print("".join(["Q" if cell else "." for cell in row]))
+
+    def solve(board, col):
+        if col == n:
+            print_solution(board)
+            print()
+            return
+
+        for i in range(n):
+            if is_safe(board, i, col, n):
+                board[i][col] = 1
+                solve(board, col + 1)
+                board[i][col] = 0
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solve(board, 0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        solve_nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
