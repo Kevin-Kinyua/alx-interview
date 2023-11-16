977de9a6ddd84114bf8e70eed34bd79aa5be3aa7
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-16 17:20:34 +0300

    rm main_0.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,59 +1,20 @@
 #!/usr/bin/python3
-"""_summary_
-"""
-
-
-def transpose_matrix(matrix, n):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
-    """
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-
-def reverse_matrix(matrix):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
-    """
-    for row in matrix:
-        row.reverse()
+"""Rotate 2D Matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
     """
-    n = len(matrix)
-    # print(n)
-
-    """sample matrix
-    1 2 3
-    4 5 6
-    7 8 9
+    function that rotates a 2D matrix
     """
-
-    # transpose matrix
-    """
-    1 4 7
-    2 5 8
-    3 6 9
-    """
-
-    transpose_matrix(matrix, n)
-
-    # reverse matrix
-    """
-    7 4 1
-    8 5 2
-    9 6 3
-    """
-    reverse_matrix(matrix)
-
-    return matrix
+    left = 0
+    right = len(matrix) - 1
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topleft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topleft
+        left += 1
+        right -= 1
