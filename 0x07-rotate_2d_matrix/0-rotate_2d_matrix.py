69f49a1726c54d2d76a5bd644b63b5421b2d8480
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-16 17:31:10 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+
 def rotate_2d_matrix(matrix):
     """
     function that rotates a 2D matrix
     """
-    n = len(matrix)
-    for layer in range(n // 2):
-        left = layer
-        right = n - 1 - layer
-        for i in range(left, right):
-            top, bottom = layer, n - 1 - layer
-            topleft = matrix[top][i]
-            matrix[top][i] = matrix[bottom - (i - left)][left]
-            matrix[bottom - (i - left)][left] = matrix[bottom][right - (i - left)]
-            matrix[bottom][right - (i - left)] = matrix[top + (i - left)][right]
-            matrix[top + (i - left)][right] = topleft
+    left = 0
+    right = len(matrix) - 1
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topleft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topleft
+        left += 1
+        right -= 1
