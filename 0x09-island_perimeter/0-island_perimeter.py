a1a2d76ab0d2fdf2e5e61b0a135bf20e9f6f1890
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-30 17:35:25 +0300

    Correction
    corrections to code errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,31 +2,27 @@
 """Island Perimeter """
 
 def island_perimeter(grid):
-    if not grid or not grid[0]:
-        return 0
+    """Calculate the perimeter of the island in the grid.
 
-    rows, cols = len(grid), len(grid[0])
+    Args:
+        grid (list[list[int]]): A 2D grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    num_rows = len(grid)
+    num_cols = len(grid[0])
     perimeter = 0
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if grid[row][col] == 1:
+                perimeter += 4  # Start with 4 sides
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
+                # Check left neighbor
+                if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
 
+                # Check above neighbor
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
     return perimeter
-
-# Example usage
-grid = [
-     [0, 0, 0, 0, 0, 0],
-     [0, 1, 0, 0, 0, 0],
-     [0, 1, 0, 0, 0, 0],
-     [0, 1, 1, 1, 0, 0],
-     [0, 0, 0, 0, 0, 0]
-]
-
-result = island_perimeter(grid)
-print(result)
