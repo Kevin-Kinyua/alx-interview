b06d470a6cf6531b058e3157fb3923fe2fbc7f9a
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-30 17:40:28 +0300

    Update 0-island_perimeter.py
    correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,27 +2,39 @@
 """Island Perimeter """
 
 def island_perimeter(grid):
-    """Calculate the perimeter of the island in the grid.
+    """ island_perimeter()
 
-    Args:
-        grid (list[list[int]]): A 2D grid representing the island.
+    Parameters:
+      - grid: Grid representing the island for which we want the perimeter.
 
     Returns:
-        int: The perimeter of the island.
+      - perimeter of the island.
     """
-    num_rows = len(grid)
-    num_cols = len(grid[0])
-    perimeter = 0
-    for row in range(num_rows):
-        for col in range(num_cols):
+    def get_perimeter(i, j):
+        """ get_perimeter()
+
+        Parameters:
+          - i: current row being evaluated
+          - j: current column being evaluated
+
+        Returns:
+          - 1, 0 or sum of perimeter of the current cell under consideration.
+        """
+        if (i < 0 or i > len(grid) - 1) or (j < 0 or j > len(grid[i]) - 1):
+            return 1
+        if grid[i][j] == 0:
+            return 1
+        if grid[i][j] == 'v':
+            return 0
+        grid[i][j] = 'v'
+        return (get_perimeter(i, j-1) + get_perimeter(i-1, j)
+                + get_perimeter(i, j+1) + get_perimeter(i+1, j))
+
+    result = 0
+    if type(grid) != list:
+        return 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter += 4  # Start with 4 sides
-
-                # Check left neighbor
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
-
-                # Check above neighbor
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
-    return perimeter
+                result = get_perimeter(row, col)
+    return result
